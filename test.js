887965273eae3c5c9d630ae3e5898e010d38875c
zemlyansky <anton.zemlyansky@gmail.com>
2017-09-07 13:03:43 +0300

    Update tests

--- test.js
+++ test.js
@@ -1,5 +1,24 @@
-var m = require('./index')
+var ava = require('ava')
+var m = require('.')
+var k = [
+  ['', 0],
+  ['0', 3530670207],
+  ['01', 1642882560],
+  ['012', 3966566284],
+  ['0123', 3558446240],
+  ['01234', 433070448],
+  ['Orange', 1637794643],
+  ['Ensemble-based online learning algorithm', 1450795871],
+  ['^123438*&6345sjfu25', 3023040079]
+]
 
+k.forEach((pair) => {
+  ava('Key: ' + pair[0], (t) => {
+    t.is(m.hash32(pair[0]), pair[1])
+  })
+})
+
+/*
 if (
   (m.hash32('') === 0) &&
   (m.hash32('0') === 3530670207) &&
@@ -13,3 +32,4 @@ if (
 } else {
   console.log('Algorithm error')
 }
+*/
